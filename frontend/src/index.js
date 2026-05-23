a94832cd3246a99ef0e66e7a99fdf112c30b4af0
ananthuN6 <ananthu@nalashaa.com>
2026-05-23 13:15:11 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { PublicClientApplication, EventType } from '@azure/msal-browser';
+import { msalConfig } from './auth/msalConfig';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+const msalInstance = new PublicClientApplication(msalConfig);
+
+msalInstance.initialize().then(() => {
+  msalInstance.handleRedirectPromise().catch(() => {});
+
+  if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
+    msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
+  }
+
+  msalInstance.addEventCallback((event) => {
+    if (event.eventType === EventType.LOGIN_SUCCESS && event.payload?.account) {
+      msalInstance.setActiveAccount(event.payload.account);
+    }
+  });
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App instance={msalInstance} />
+    </React.StrictMode>
+  );
+});
