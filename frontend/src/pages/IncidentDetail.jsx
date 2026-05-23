655caf2d1fa4ea029bed3d9fc53df3a70bba3006
ananthuN6 <ananthu@nalashaa.com>
2026-05-23 13:00:46 +0530

    Update IncidentDetail.jsx

--- frontend/src/pages/IncidentDetail.jsx
+++ frontend/src/pages/IncidentDetail.jsx
@@ -1,4 +1,5 @@
 // frontend/src/pages/IncidentDetail.jsx
+/* eslint-disable */
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useApp, useCurrentUser } from '../context/AppContext';
